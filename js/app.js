ba507d7acac44dd286644f275a51d7204db3d15b
alfredo <you@example.com>
2018-11-21 00:54:31 -0800

    started timer and reset

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
-/*
- * Create a list that holds all of your cards
- */
+//  Programming cards through javascript format from Mike webinar
+//An array holds all of the cards
+let cards = ['fa-diamond', 'fa-diamond',
+              'fa-paper-plane-o', 'fa-paper-plane-o',
+              'fa-anchor', 'fa-anchor',
+              'fa-bolt', 'fa-bolt',
+              'fa-cube', 'fa-cube',
+              'fa-leaf', 'fa-leaf',
+              'fa-bicycle', 'fa-bicycle',
+              'fa-bomb', 'fa-bomb'];
 
+//template literal used in a function that generates the cards programatically
+function generateCard(card) {
+    return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
+}
 
 /*
  * Display the cards on the page
@@ -12,7 +23,7 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -26,40 +37,182 @@ function shuffle(array) {
 }
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-const cards = document.querySelectorAll('.card');
-const openCards = [];
+ //function to initiate the game
+function initiateGame() {
+  //stores the unorderd list with the class .deck from the HTML file, where we will put cards
+  let deck = document.querySelector('.deck');
+  let cardHTML = shuffle(cards).map(function(card) {
+    //calls generateCard function for each card
+    return generateCard(card);
+  });
+  deck.innerHTML = cardHTML.join('');
+}
+
+initiateGame();
+gameStopwatch();
+
+// ***** Global Scope Variables ***** //
+
+
+//variable hold all cards
+let allCards = document.querySelectorAll('.card');
+//Array to hold cards that are open. Initially empty.
+let openCards = [];
+//moves counter - begins at 0
+let moves = 0;
+//selects the moves counter in index.html
+let movesCounter = document.querySelector('.moves');
+//selects the restart game "fa-repeat" icon
+let restartGame = document.querySelector('.fa-repeat');
+//selects minutes section of HTML stopwatch
+let displayMinutes = document.querySelector('.minutes');
+//selects seconds section of HTML stopwatch
+let displaySeconds = document.querySelector('.seconds');
+//variable for timer to keep track of elapsed milliseconds
+let milliseconds = 0;
+let hour = 0;
+let minutes = 0;
+let seconds = 0;
+//variable to track matched cards to evaluate if game has been won
+let matched = 0;
+//need 8 pairs to win the gameStopwatch
+const winningPairs = 8;
+//for gameStopwatch
+var interval;
+
+ //event listener for restart game button
+ restartGame.addEventListener('click', function(e) {
+   //removes any open, show or match classes from cards
+   allCards.forEach(function(card) {
+     card.classList.remove('open', 'show', 'match');
+   });
+   //adds stars back to star rating
+   let stars = document.querySelectorAll('.fa-star');
+  stars.forEach(function(element) {
+      element.style.display = 'inline-block';
+  })
+   initiateGame();
+   stopTimer();
+   console.log('Reinitiate game');
+   //resets # of moves when reset button is clicked
+   moves = 0;
+   movesCounter.innerText = moves;
+   //moves matched cards tracker back to 0
+   matched = 0;
+   milliseconds = 0;
+   openCards = [];
+ });
+
+ //game stopwatch function
+ function gameStopwatch() {
+    //clearInterval(interval); Supposed to reset stopwatch, but not doing anything
+    //setInterval evaluates an expression at specified intervals (every 1000 milliseconds/1 second here)
+      interval = setInterval(function() {
+      seconds++;
+      if(seconds == 60) {
+          minutes++;
+          seconds = 0;
+      }
+      formatTimer();
+    }, 1000);
+ }
+
+ function stopTimer() {
+     clearInterval(interval);
+     displaySeconds.innerHTML = gameStopwatch();
+ }
+
+ function formatTimer() {
+     let sec = seconds > 9 ? String(seconds) : "0" + String(seconds);
+     let min = minutes > 9 ? String(minutes) : "0" + String(minutes);
+    //  displayMinutes.innerHTML = min;
+     displaySeconds.innerHTML = min + ':' + sec;
+ }
+
+//converts milliseconds to seconds and minutes to be displayed
+//  function convertSeconds(milliseconds) {
+//     let hour = 0;
+//     let minutes = 0;
+//     let seconds = 0;
+// //    let minutes = Math.floor(((milliseconds % 864000) % 3600) / 60);
+// //    let seconds = ((milliseconds % 86400) % 3600) % 60;
+// //    displayMinutes.innerHTML = minutes;
+// //    if(seconds < 10) {
+// //       displaySeconds.innerHTML = "0" + seconds;
+// //  } else {
+// //       displaySeconds.innerHTML = seconds;
+// //     }
 
 
 
-cards.forEach((card) => {
-    //  When click on card add the classes
-    card.addEventListener('click', (e) => {
 
-        if(!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')){
-            openCards.push(card);
-            card.classList.add('open', 'show'); 
+//  }
 
-            if(openCards.length === 2) {
-                console.log('Fire');
-                setTimeout(() => {
-                    openCards.forEach((card) => {
-                        card.classList.remove('open', 'show');
-                    });
+//function to check how many moves have been made & change star rating
+function starRating() {
+  const three = document.querySelector('.three');
+  const two = document.querySelector('.two');
+  const one = document.querySelector('.one');
+  if (moves === 10) {
+    three.style.display = 'none';
+    console.log('moves = 10');
+  } else if (moves === 15) {
+    two.style.display = 'none';
+    console.log('moves = 15')
+  }
+}
 
-                    openCares = [];
-                }, 1000)
-            }
-        }
-    })
-})
+ //event listner for clicks on cards
+ let deck = document.querySelector('.deck');
 
+deck.addEventListener('click', event => {
+  const clickTarget = event.target;
+  if(clickTarget.classList.contains('card')) {
+    //   gameStopwatch();
+    //disables ability to click on a matched card or the same card twice
+     if (!clickTarget.classList.contains('open') && !clickTarget.classList.contains('show') && !clickTarget.classList.contains('match')) {
+         //when a card is clicked, the card gets added to the openCards array
+         openCards.push(clickTarget);
+         //adds .open and .show classes when card is clicked
+         clickTarget.classList.add('open', 'show');
+
+         //if 2 or more cards are showing, see if they are a match or not
+         //since the array gets cleared out each time, there will only be 2 cards in the array at a time
+         if (openCards.length == 2) {
+           //if the cards match, add the .match, .open & .show classes
+           if (openCards[0].dataset.card == openCards[1].dataset.card) {
+               openCards[0].classList.add('match');
+               openCards[0].classList.add('open');
+               openCards[0].classList.add('show');
+
+               openCards[1].classList.add('match');
+               openCards[1].classList.add('open');
+               openCards[1].classList.add('show');
+
+               openCards = [];
+               //Adds 1 to matched variable for each pair of matched cards
+               matched++;
+           } else {
+          //if it's not a match, hide the cards again
+           setTimeout(function() {
+             openCards.forEach(function(card) {
+               card.classList.remove('open', 'show');
+             });
+             openCards = [];
+           }, 350);
+         }
+         moves += 1;
+         movesCounter.innerText = moves;
+         if (matched === winningPairs) {
+           console.log("Game over!");
+           clearInterval(interval);
+           //displays modal Window
+           modal.style.display = 'block';
+           //writes final game stats to the modal window
+           finalStats();
+         }
+       }
+      starRating();
+     }
+   }
+});
