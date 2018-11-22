31f4c2a6cd9d9b724b45f6a9ce2a401f36fc5844
alfredo <you@example.com>
2018-11-21 20:08:13 -0800

    added stop watch & finished timer

--- js/app.js
+++ js/app.js
@@ -107,13 +107,9 @@ var interval;
  function gameStopwatch() {
     //clearInterval(interval); Supposed to reset stopwatch, but not doing anything
     //setInterval evaluates an expression at specified intervals (every 1000 milliseconds/1 second here)
-      interval = setInterval(function() {
-      seconds++;
-      if(seconds == 60) {
-          minutes++;
-          seconds = 0;
-      }
-      formatTimer();
+    var interval = setInterval(function() {
+      milliseconds++;
+      convertSeconds(Math.floor(milliseconds));
     }, 1000);
  }
 
@@ -129,24 +125,17 @@ var interval;
      displaySeconds.innerHTML = min + ':' + sec;
  }
 
-//converts milliseconds to seconds and minutes to be displayed
-//  function convertSeconds(milliseconds) {
-//     let hour = 0;
-//     let minutes = 0;
-//     let seconds = 0;
-// //    let minutes = Math.floor(((milliseconds % 864000) % 3600) / 60);
-// //    let seconds = ((milliseconds % 86400) % 3600) % 60;
-// //    displayMinutes.innerHTML = minutes;
-// //    if(seconds < 10) {
-// //       displaySeconds.innerHTML = "0" + seconds;
-// //  } else {
-// //       displaySeconds.innerHTML = seconds;
-// //     }
-
-
-
-
-//  }
+// converts milliseconds to seconds and minutes to be displayed
+function convertSeconds(milliseconds) {
+  let minutes = Math.floor(((milliseconds % 864000) % 3600) / 60);
+  let seconds = ((milliseconds % 86400) % 3600) % 60;
+  displayMinutes.innerHTML = minutes;
+  if(seconds < 10) {
+     displaySeconds.innerHTML = "0" + seconds;
+} else {
+     displaySeconds.innerHTML = seconds;
+   }
+}
 
 //function to check how many moves have been made & change star rating
 function starRating() {
